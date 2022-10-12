3fa7b958f5b5ee7a74e6ac31a429123c0b6c0f59
Shannon Sangha <sks120@ic.ac.uk>
2022-10-12 16:27:45 +0100

    DS-25 Added a normalising function

--- src/wave_model/wave_model.py
+++ src/wave_model/wave_model.py
@@ -1,5 +1,12 @@
 import numpy as np
 
+MAX_SOUND_VAL = 32767
+
+
+def normalize_sound(arr: np.ndarray) -> np.ndarray:
+    sound = arr * MAX_SOUND_VAL
+    return sound.astype(np.int16)
+
 
 class SinWave:
     def __init__(self, freq: float, amp: float):
--- src/waveview/wave.py
+++ src/waveview/wave.py
@@ -5,14 +5,14 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy_garden.graph import Graph, LinePlot
 import simpleaudio as sa
 import numpy as np
-from src.wave_model.wave_model import SinWave
+from src.wave_model.wave_model import SinWave, normalize_sound
 
 
 class RootWave(BoxLayout):
     def __init__(self, **kwargs: typing.Any):
         super(RootWave, self).__init__(**kwargs)
         self.points = []
-        self.num_samples = 10000
+        self.num_samples = 44100
         self.play.bind(on_press=self.callback)
         self.graph = Graph(border_color=[0, 1, 1, 1],
                            xmin=0, xmax=self.num_samples,
@@ -23,16 +23,21 @@ class RootWave(BoxLayout):
         self.plot_x = np.linspace(0, 1, self.num_samples)
         self.plot_y = np.zeros(self.num_samples)
         self.plot = LinePlot(color=[1, 1, 0, 1], line_width=1.5)
+        starting_freq = 400
+        starting_amp = 1
+
+        self.sin_wave = SinWave(starting_freq, starting_amp)
         self.graph.add_plot(self.plot)
-        self.update_plot(10, 100)
+        self.update_plot(starting_freq, starting_amp)
 
     def update_plot(self, freq: int, amp: int) -> None:
-        sin_wave = SinWave(freq, amp / 100.0)
-        self.points = sin_wave.get_array(self.num_samples)
+        self.sin_wave.freq = freq
+        self.sin_wave.amp = amp / 100.0
+        self.points = self.sin_wave.get_array(self.num_samples)
         self.plot.points = [(x, self.points[x]) for x in range(self.points.size)]
 
     def callback(self, arg: typing.Any) -> None:
-        sa.play_buffer(self.points, 2, 2, 44100)
+        sa.play_buffer(normalize_sound(self.points), 1, 2, 44100)
 
 
 class WaveApp(App):

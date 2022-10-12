f2410e1bcab455ebb35aaf9969761ada7ffb1da2
Shannon Sangha <sks120@ic.ac.uk>
2022-10-12 14:06:40 +0100

    DS-22 Basic sound model with tests

---
+++ src/wave_model/wave_model.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+class SinWave:
+    def __init__(self, freq: int, amp: int):
+        self.freq = freq
+        self.amp = amp
+
+    def get_array(self, samples: int) -> np.array:
+        x = np.linspace(0, 2 * np.pi, samples)
+        return self.amp * np.sin(x * self.freq)

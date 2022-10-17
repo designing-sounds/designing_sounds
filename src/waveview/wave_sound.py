bc3c01bbd800e9109691b960166343e0b1a80fb2
Tom Woodley <thwoodley@gmail.com>
2022-10-17 09:50:31 +0100

    fix start playing

--- src/waveview/wave_sound.py
+++ src/waveview/wave_sound.py
@@ -2,7 +2,6 @@ import numpy as np
 import pyaudio
 from src.wave_model.wave_model import normalize_sound
 from kivy.core.window import Window
-from kivy.uix.popup import Popup
 
 
 class WaveSound:
@@ -14,6 +13,7 @@ class WaveSound:
         self.p = pyaudio.PyAudio()
         self.sound = np.array([]).astype(np.float32).tostring()
         self.stream = self.p.open(format=pyaudio.paFloat32, channels=1, rate=self.sample_rate, output=True, stream_callback=self.callback, frames_per_buffer=int(self.sample_rate*time))
+        self.stream.stop_stream()
         Window.bind(on_request_close=self.shutdown_audio)
 
     def callback(self, in_data, frame_count, time_info, flag):

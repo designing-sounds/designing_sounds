f370e1b5a54a5b3d8d54eeebed36e25dd4533244
normateos <nm920@ic.ac.uk>
2022-10-27 17:04:09 +0100

    DS-63 Selected color on power spectrum buttons

--- src/wave_view/wave.py
+++ src/wave_view/wave.py
@@ -57,6 +57,8 @@ class RootWave(BoxLayout):
         self.power_spectrum_graph.add_plot(self.power_plot)
 
         self.power_buttons = []
+        self.selected_button_color = [0, 1, 1, 1]
+        self.unselected_button_color = [1, 1, 1, 1]
         self.harmonic_list = np.zeros((self.max_harmonics, 3))
         self.press_button_add(None)
 
@@ -87,6 +89,7 @@ class RootWave(BoxLayout):
 
     def update_display_power_spectrum(self, harmonic_index: int, change_harmonic: bool):
         self.do_not_change_waveform = change_harmonic
+        self.change_selected_power_spectrum_button(harmonic_index)
         self.harmonic_list[self.current_harmonic_index] = np.array(
             [self.mean.value, self.sd.value, self.harmonic_samples.value])
         self.current_harmonic_index = harmonic_index
@@ -101,12 +104,21 @@ class RootWave(BoxLayout):
 
     def add_power_spectrum_button(self) -> None:
         self.num_harmonics += 1
-        button = Button(text=str(self.num_harmonics), size_hint=(0.1, 1))
-        button.bind(on_press=self.press_button_display_power_spectrum)
+        self.power_button_colors.append(self.selected_button_color)
+        button = Button(
+            text=str(self.num_harmonics),
+            size_hint=(0.1, 1),
+            background_color=self.selected_button_color,
+            on_press=self.press_button_display_power_spectrum
+        )
         self.power_buttons.append(button)
         self.ids.power_spectrum_buttons.add_widget(button)
         self.harmonic_list[self.num_harmonics - 1] = np.array([self.mean.max // 2, 1, self.harmonic_samples.max // 2])
         self.update_display_power_spectrum(self.num_harmonics - 1, False)
+
+    def change_selected_power_spectrum_button(self, new_selection: int):
+        self.power_buttons[self.current_harmonic_index].background_color = self.unselected_button_color
+        self.power_buttons[new_selection].background_color = self.selected_button_color
 
 
 class WaveApp(App):

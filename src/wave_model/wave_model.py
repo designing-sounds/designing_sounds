5c1351a0713ab3f0fc6c093ffa6d66dce876ce3c
Agustin Sotero Bourel <52137971+AgustinSF@users.noreply.github.com>
2022-11-02 13:22:03 +0000

    DS-69 add multiple harmonics

--- src/wave_model/wave_model.py
+++ src/wave_model/wave_model.py
@@ -9,31 +9,12 @@ from numpy.linalg import inv
 class PowerSpectrum:
     def __init__(self, max_harmonics: int, max_samples_per_harmonic: int):
         self.max_samples_per_harmonic = max_samples_per_harmonic
-        self.harmonics = []
-
-        for i in range(max_harmonics):
-            self.harmonics.append(None)
-            self.update_harmonic(i, 0, 0, max_samples_per_harmonic)
+        self.harmonics = np.zeros((max_harmonics, self.max_samples_per_harmonic), dtype=np.float32)
 
     def update_harmonic(self, harmonic_index, mean: int, std: float, num_harmonic_samples: int) -> None:
-        # new_freqs = np.random.randn(num_harmonic_samples) * std + mean
-        # freqs = np.zeros(self.max_samples_per_harmonic)
-        # freqs[:num_harmonic_samples] = new_freqs
-        # kern_sum = None
-        # for freq in freqs:
-        #     kern = gpflow.kernels.Periodic(base_kernel=gpflow.kernels.SquaredExponential())
-        #     kern.period.assign(1 / freq) if freq != 0 else kern.period.assign(0.01)
-        #     kern.base_kernel.lengthscales.assign(0.5)
-        #     sqexp = gpflow.kernels.SquaredExponential()
-        #     sqexp.lengthscales.assign(7.0)
-        #     kern = kern * sqexp
-        #
-        #     if not kern_sum:
-        #         kern_sum = kern
-        #     kern_sum += kern
-        # assert kern_sum
-        # self.harmonics[harmonic_index] = kern_sum
-        pass
+        freqs = np.random.randn(num_harmonic_samples) * std + mean
+        self.harmonics[harmonic_index] = np.zeros(self.max_samples_per_harmonic)
+        self.harmonics[harmonic_index, :num_harmonic_samples] = freqs
 
 
 class SoundModel:
@@ -52,12 +33,21 @@ class SoundModel:
         self.Y = None
         self.interpolate_points([])
 
-    def get_power_spectrum_histogram(self, harmonic_index: int, num_bins: int) -> typing.List[
-        typing.Tuple[float, float]]:
-        return [(0, 0)]
+    def get_power_spectrum_histogram(self, harmonic_index: int, num_bins: int) -> typing.List[typing.Tuple[float, float]]:
+        self.lock.acquire()
+        freqs = self.power_spectrum.harmonics[harmonic_index]
+        freqs = freqs[np.nonzero(freqs)]
+        histogram, bin_edges = np.histogram(freqs, self.max_freq // 2, range=(0.1, 1000))
+        self.lock.release()
+        return list(zip(bin_edges, histogram))
 
     def get_sum_all_power_spectrum_histogram(self) -> typing.List[typing.Tuple[float, float]]:
-        return [(0, 0)]
+        self.lock.acquire()
+        freqs = self.power_spectrum.harmonics.flatten()
+        freqs = freqs[np.nonzero(freqs)]
+        histogram, bin_edges = np.histogram(freqs, self.max_freq // 2, range=(0.1, 1000))
+        self.lock.release()
+        return list(zip(bin_edges, histogram))
 
     def interpolate_points(self, points: typing.List[typing.Tuple[float, float]]):
         self.lock.acquire()
@@ -68,6 +58,7 @@ class SoundModel:
     def update_power_spectrum(self, harmonic_index: int, mean: int, std: float, num_harmonic_samples: int) -> None:
         self.lock.acquire()
         self.power_spectrum.update_harmonic(harmonic_index, mean, std, num_harmonic_samples)
+        self.samples_per_harmonic[harmonic_index] = num_harmonic_samples
         self.lock.release()
 
     def model_sound(self, sample_rate: int, chunk_duration: float, start_time: float) -> np.ndarray:
@@ -76,10 +67,7 @@ class SoundModel:
         x = np.linspace(start_time, start_time + chunk_duration, int(chunk_duration * sample_rate), endpoint=False)
 
         self.lock.acquire()
-        # predict_stats = self.m.predict_f(x[:, None])
-        # sound, _ = [d.numpy() for d in predict_stats]
-        # sound = np.asarray(sound.flatten(), dtype=np.float32)
-        sound = self.matrix_covariance(x, self.X) @ inv(self.matrix_covariance(self.X, self.X)) @ self.Y.T
+        sound = self.matrix_covariance(x, self.X, 440) @ inv(self.matrix_covariance(self.X, self.X, 440)) @ self.Y.T
         sound = np.asarray(sound, dtype=np.float32)
         self.lock.release()
 
@@ -88,11 +76,11 @@ class SoundModel:
     def covariance(self, x1, x2, period, lengthscale):
         return np.exp(-0.5 * np.square(np.sin(np.pi * (x1 - x2) / period)) / lengthscale)
 
-    def matrix_covariance(self, x1, x2):
+    def matrix_covariance(self, x1, x2, freq):
         n = len(x1)
         m = len(x2)
         res = np.zeros((n, m))
         for i in range(n):
             for j in range(m):
-                res[i, j] = self.covariance(x1[i], x2[j], 1 / 440, 1)
+                res[i, j] = self.covariance(x1[i], x2[j], 1 / freq, 1)
         return res

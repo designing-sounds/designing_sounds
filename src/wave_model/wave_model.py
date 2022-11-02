389e30300768a1fd44e6fa85d538cc476c23efd2
Agustin Sotero Bourel <52137971+AgustinSF@users.noreply.github.com>
2022-11-02 10:46:50 +0000

    DS-69 implement multiple kernels per harmonic

--- src/wave_model/wave_model.py
+++ src/wave_model/wave_model.py
@@ -8,12 +8,30 @@ import gpflow
 class PowerSpectrum:
     def __init__(self, max_harmonics: int, max_samples_per_harmonic: int):
         self.max_samples_per_harmonic = max_samples_per_harmonic
-        self.harmonics = np.zeros((max_harmonics, self.max_samples_per_harmonic), dtype=np.float32)
+        self.harmonics = []
+
+        for i in range(max_harmonics):
+            self.harmonics.append(None)
+            self.update_harmonic(i, 0, 0, max_samples_per_harmonic)
 
     def update_harmonic(self, harmonic_index, mean: int, std: float, num_harmonic_samples: int) -> None:
-        freqs = np.random.randn(num_harmonic_samples) * std + mean
-        self.harmonics[harmonic_index] = np.zeros(self.max_samples_per_harmonic)
-        self.harmonics[harmonic_index, :num_harmonic_samples] = freqs
+        new_freqs = np.random.randn(num_harmonic_samples) * std + mean
+        freqs = np.zeros(self.max_samples_per_harmonic)
+        freqs[:num_harmonic_samples] = new_freqs
+        kern_sum = None
+        for freq in freqs:
+            kern = gpflow.kernels.Periodic(base_kernel=gpflow.kernels.SquaredExponential())
+            kern.period.assign(1 / freq) if freq != 0 else kern.period.assign(0.01)
+            kern.base_kernel.lengthscales.assign(0.5)
+            sqexp = gpflow.kernels.SquaredExponential()
+            sqexp.lengthscales.assign(7.0)
+            kern = kern * sqexp
+
+            if not kern_sum:
+                kern_sum = kern
+            kern_sum += kern
+        assert kern_sum
+        self.harmonics[harmonic_index] = kern_sum
 
 
 class SoundModel:
@@ -26,30 +44,16 @@ class SoundModel:
         self.power_spectrum = PowerSpectrum(self.max_harmonics, self.max_samples_per_harmonic)
         self.samples_per_harmonic = np.zeros(self.max_harmonics)
         self.lock = threading.Lock()
-        kern = gpflow.kernels.Periodic(base_kernel=gpflow.kernels.SquaredExponential())
-        kern.period.assign(0.02)
-        kern.base_kernel.lengthscales.assign(0.5)
-        sqexp = gpflow.kernels.SquaredExponential()
-        sqexp.lengthscales.assign(7.0)
-        self.kern = kern * sqexp
+        self.kern = None
         self.m = None
         self.interpolate_points([])
 
-    def get_power_spectrum_histogram(self, harmonic_index: int, num_bins: int) -> typing.List[typing.Tuple[float, float]]:
-        self.lock.acquire()
-        freqs = self.power_spectrum.harmonics[harmonic_index]
-        freqs = freqs[np.nonzero(freqs)]
-        histogram, bin_edges = np.histogram(freqs, self.max_freq // 2, range=(0.1, 1000))
-        self.lock.release()
-        return list(zip(bin_edges, histogram))
+    def get_power_spectrum_histogram(self, harmonic_index: int, num_bins: int) -> typing.List[
+        typing.Tuple[float, float]]:
+        return [(0, 0)]
 
     def get_sum_all_power_spectrum_histogram(self) -> typing.List[typing.Tuple[float, float]]:
-        self.lock.acquire()
-        freqs = self.power_spectrum.harmonics.flatten()
-        freqs = freqs[np.nonzero(freqs)]
-        histogram, bin_edges = np.histogram(freqs, self.max_freq // 2, range=(0.1, 1000))
-        self.lock.release()
-        return list(zip(bin_edges, histogram))
+        return [(0, 0)]
 
     def interpolate_points(self, points: typing.List[typing.Tuple[float, float]]):
         if not points:
@@ -57,27 +61,21 @@ class SoundModel:
 
         X, Y = [x for (x, _) in points], [y for (_, y) in points]
         X, Y = np.array(X)[:, None], np.array(Y)[:, None]
-        m = gpflow.models.GPR((X, Y), self.kern)
+        m = gpflow.models.GPR((X, Y), np.sum(self.power_spectrum.harmonics))
         m.likelihood.variance.assign(1e-2)
         self.m = m
 
     def update_power_spectrum(self, harmonic_index: int, mean: int, std: float, num_harmonic_samples: int) -> None:
         self.lock.acquire()
         self.power_spectrum.update_harmonic(harmonic_index, mean, std, num_harmonic_samples)
-        self.samples_per_harmonic[harmonic_index] = num_harmonic_samples
-        self.phases = np.asarray(np.random.uniform(0, self.max_freq, self.max_harmonics * self.max_samples_per_harmonic), dtype=np.float32)
         self.lock.release()
-
-    def calculate_sins(self, x):
-        freqs = self.power_spectrum.harmonics.flatten()
-        sins = np.sin((x[:, None]) * 2 * np.pi * freqs)
-        return sins
 
     def model_sound(self, sample_rate: int, chunk_duration: float, start_time: float) -> np.ndarray:
         x = np.linspace(start_time, start_time + chunk_duration, int(chunk_duration * sample_rate), endpoint=False)
 
         self.lock.acquire()
         predict_stats = self.m.predict_f(x[:, None])
+        self.m.kernel
         sound, _ = [d.numpy() for d in predict_stats]
         sound = np.asarray(sound.flatten(), dtype=np.float32)
         self.lock.release()

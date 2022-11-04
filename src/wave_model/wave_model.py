a2faea370c981bdc73c2188314a0f2c17e53cd92
Kartik Iyer <kartikiyer1011@gmail.com>
2022-11-04 09:42:47 +0000

    fix indexing in model_sound

--- src/wave_model/wave_model.py
+++ src/wave_model/wave_model.py
@@ -71,7 +71,7 @@ class SoundModel:
 
         self.lock.acquire()
         if only_add_harmonic:
-            freq = self.power_spectrum.harmonics[self.current_harmonic_index][0]
+            freq = self.power_spectrum.harmonics[self.current_harmonic_index, 0]
             self.power_spectrum.harmonic_sounds[self.current_harmonic_index] = np.array(self.calculate_sound(x, freq, 1, 1))
         else:
             self.recompute_harmonic_sounds(x, chunk_duration, sample_rate)

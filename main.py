c007599b476bc51c17d4ec924a71a7c338acc2c2
Shannon Sangha <sks120@ic.ac.uk>
2022-11-30 10:59:27 +0000

    DS-92 disable resizing of whole app

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-import sys
-
-import kivy
 from kivy.config import Config
-Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
-Config.write()
 
 from src.wave_controller import wave
 from kivy.core.window import Window
 
+Window.maximize()
+max_size = Window.system_size
+Window.size = (max_size[0], max_size[1])
+
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
+Config.set('graphics', 'resizable', False)
+Config.write()
 
 if __name__ == '__main__':
     Window.maximize()

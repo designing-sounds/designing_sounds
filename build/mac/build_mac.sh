fba187c9c90cba5b9b200514002b249ed9a6c3a5
Tom Woodley <thwoodley@gmail.com>
2022-12-30 11:23:18 +0000

    added matchfile

--- build/mac/build_mac.sh
+++ build/mac/build_mac.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 pyinstaller -y --clean Sounds.spec
+security find-identity -p basic -v
 sudo codesign -s - --force --all-architectures --timestamp --deep dist/sounds.app
 pushd dist
 hdiutil create ./sounds.dmg -srcfolder sounds.app -ov

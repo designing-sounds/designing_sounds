71664b54bc4bed0a4205543f0a077f582ab89429
Tom Woodley <thwoodley@gmail.com>
2023-01-04 12:12:34 +0000

    removed unused binaries

--- build/mac/build_mac.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pyinstaller -y --clean Sounds.spec
-security find-identity -p basic -v
-sudo codesign -s - --force --all-architectures --timestamp --deep dist/sounds.app
-pushd dist
-hdiutil create ./sounds.dmg -srcfolder sounds.app -ov
-popd
-cp ./dist/sounds.dmg .

41dee26676b5328237adde63bde47d7559b44642
Agustin Sotero Bourel <52137971+AgustinSF@users.noreply.github.com>
2023-01-07 12:39:10 +0100

    DS-154 refactored priors and changed frequency slider min value to 1

---
+++ test/wave_model/test_priors.py
@@ -0,0 +1,24 @@
+from src.wave_model.priors import PeriodicPrior, SquaredExpPrior, MultPrior
+import numpy as np
+import unittest
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+class TestPriors(unittest.TestCase):
+
+    def setUp(self):
+        self.tolerance = 3 * 1e-5
+        self.approx_dim = 50
+
+    def test_periodic_kernel(self):
+        assert round(PeriodicPrior(50).kernel(0.497, 100, 3, 1.2, 0, 0), 9) == 7.170423242
+
+    def test_periodic_covariance_matrix(self):
+        expected = np.array([[[14.45854572, 2.82116777], [14.45854572, 2.821167767]]])
+        np.testing.assert_allclose(expected, PeriodicPrior(50).covariance_matrix(np.array([1, -3]), np.array([0.19, 5.12]), [442], [5], [0.06], [0], [0]), self.tolerance)
+
+
+
+
--- test/wave_model/test_wave_model.py
+++ test/wave_model/test_wave_model.py
@@ -26,3 +26,7 @@ class TestSoundModel(unittest.TestCase):
         test = test.flatten()
         expected = self.sound_model.model_sound(sample_rate, 1, 0)
         np.testing.assert_allclose(expected, test, self.tolerance)
+
+
+
+

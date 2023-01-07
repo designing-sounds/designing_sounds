37b6df7584ded6168231be51bd5518034edede43
Kartik Iyer <kartikiyer1011@gmail.com>
2023-01-07 16:18:38 +0000

    Add mult tests

--- test/wave_model/test_priors.py
+++ test/wave_model/test_priors.py
@@ -17,7 +17,9 @@ class TestPriors(unittest.TestCase):
 
     def test_periodic_covariance_matrix(self):
         expected = np.array([[[14.45854572, 2.82116777], [14.45854572, 2.821167767]]])
-        np.testing.assert_allclose(expected, PeriodicPrior(50).covariance_matrix(np.array([1, -3]), np.array([0.19, 5.12]), [442], [5], [0.06], [0], [0]), self.tolerance)
+        np.testing.assert_allclose(expected,
+                                   PeriodicPrior(50).covariance_matrix(np.array([1, -3]), np.array([0.19, 5.12]), [442],
+                                                                       [5], [0.06], [0], [0]), self.tolerance)
 
     def test_squared_exp_kernel(self):
         assert round(SquaredExpPrior(50).kernel(0.497, 100, 3, 1.2, 0, 0), 9) == 8.260272386
@@ -25,8 +27,16 @@ class TestPriors(unittest.TestCase):
     def test_squared_exp_covariance_matrix(self):
         expected = [[[6.650516e-39, 0.000000e+00], [0.000000e+00, 0.000000e+00]]]
         np.testing.assert_allclose(expected,
-                                   SquaredExpPrior(50).covariance_matrix(np.array([1, -3]), np.array([0.19, 5.12]), [442],
-                                                                       [5], [0.06], [0], [0]), self.tolerance)
+                                   SquaredExpPrior(50).covariance_matrix(np.array([1, -3]), np.array([0.19, 5.12]),
+                                                                         [442],
+                                                                         [5], [0.06], [0], [0]), self.tolerance)
 
+    def test_mult_kernel(self):
+        assert round(MultPrior(50).kernel(0.497, 100, 3, 1.2, 2, 3), 9) == 28.290790322
 
-
+    def test_mult_covariance_matrix(self):
+        expected = [[[8.976230e-34, 0.000000e+00], [7.908304e-34, 0.000000e+00]]]
+        np.testing.assert_allclose(expected,
+                                   MultPrior(50).covariance_matrix(np.array([3, -3]), np.array([0.19, 5.12]),
+                                                                   [12],
+                                                                   [5], [0.06], [6], [3]), self.tolerance)

03c78a376fdd9753f69358aacf9f5b6ec3727d91
Kartik Iyer <kartikiyer1011@gmail.com>
2023-01-07 16:06:38 +0000

    Add kernel and covariance matrix tests

--- test/wave_model/test_priors.py
+++ test/wave_model/test_priors.py
@@ -19,6 +19,14 @@ class TestPriors(unittest.TestCase):
         expected = np.array([[[14.45854572, 2.82116777], [14.45854572, 2.821167767]]])
         np.testing.assert_allclose(expected, PeriodicPrior(50).covariance_matrix(np.array([1, -3]), np.array([0.19, 5.12]), [442], [5], [0.06], [0], [0]), self.tolerance)
 
+    def test_squared_exp_kernel(self):
+        assert round(SquaredExpPrior(50).kernel(0.497, 100, 3, 1.2, 0, 0), 9) == 8.260272386
+
+    def test_squared_exp_covariance_matrix(self):
+        expected = [[[6.650516e-39, 0.000000e+00], [0.000000e+00, 0.000000e+00]]]
+        np.testing.assert_allclose(expected,
+                                   SquaredExpPrior(50).covariance_matrix(np.array([1, -3]), np.array([0.19, 5.12]), [442],
+                                                                       [5], [0.06], [0], [0]), self.tolerance)
 
 
 

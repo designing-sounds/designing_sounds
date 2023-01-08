95bce9603091125820fd29c940ccbd4134312ac9
AgustinSotero <52137971+AgustinSF@users.noreply.github.com>
2023-01-08 17:54:29 +0000

    DS-174 refactored and added typing to model files

--- src/wave_model/power_spectrum.py
+++ src/wave_model/power_spectrum.py
@@ -13,56 +13,54 @@ class PowerSpectrum:
         self.priors = [MultPrior(approx_num), PeriodicPrior(approx_num)]
         self.prior = self.priors[0]
 
-    def change_kernel(self, idx: int):
+    def change_kernel(self, idx: int) -> None:
         old_weights = self.prior.weights
         self.prior = self.priors[idx]
         self.prior.weights = old_weights
         self.prior.update(self.get_freqs(), self.get_periodic_lengthscales(), self.get_periodic_sds())
 
-    def get_freqs(self):
+    def get_freqs(self) -> np.ndarray:
         return self.stats[:, 0]
 
-    def get_periodic_sds(self):
+    def get_periodic_sds(self) -> np.ndarray:
         return self.stats[:, 1]
 
-    def get_periodic_lengthscales(self):
+    def get_periodic_lengthscales(self) -> np.ndarray:
         return self.stats[:, 2]
 
-    def get_squared_sds(self):
+    def get_squared_sds(self) -> np.ndarray:
         return self.stats[:, 3]
 
-    def get_squared_lengthscales(self):
+    def get_squared_lengthscales(self) -> np.ndarray:
         return self.stats[:, 4]
 
-    def clear_all(self):
+    def clear_all(self) -> None:
         self.stats = np.empty((0, 5), dtype=np.float32)
         self.num_harmonics_per_spectrum = np.zeros(self.max_harmonics_per_spectrum, dtype=int)
         self.prior.weights = None
 
-    def update_harmonic(self, power_spectrum_index, mean: float, periodic_sd: float, periodic_lengthscale: float,
-                        squared_sd: float, squared_lengthscale: float,
-                        curr_harmonic_index: int) -> None:
+    def update_harmonic(self, power_spectrum_index: int, freq: float, periodic_sd: float, periodic_lengthscale: float,
+                        squared_sd: float, squared_lengthscale: float, num_harmonics: int) -> None:
         idx = np.sum(self.num_harmonics_per_spectrum[:power_spectrum_index])
         curr_num_harmonics = self.num_harmonics_per_spectrum[power_spectrum_index]
         approx_dim = self.prior.approx_dim
-        for i in range(curr_num_harmonics, curr_harmonic_index):
-            self.stats = np.insert(self.stats, idx + i,
-                                   np.array([mean * (i + 1), periodic_sd, periodic_lengthscale, squared_sd,
-                                             squared_lengthscale]), axis=0)
+        for i in range(curr_num_harmonics, num_harmonics):
+            self.stats = np.insert(self.stats, idx + i, np.array([freq * (i + 1), periodic_sd, periodic_lengthscale,
+                                                                  squared_sd, squared_lengthscale]), axis=0)
             if self.prior.weights is None:
                 self.prior.weights = np.asarray(np.random.randn(1, approx_dim), dtype=np.float32)
             else:
                 self.prior.weights = np.insert(self.prior.weights, idx + i, np.random.randn(approx_dim), axis=0)
 
-        self.delete_harmonics(power_spectrum_index, curr_harmonic_index, curr_num_harmonics)
+        self.delete_harmonics(power_spectrum_index, num_harmonics, curr_num_harmonics)
 
-        for i in range(curr_harmonic_index):
-            self.stats[idx + i] = np.array([mean * (i + 1), periodic_sd, periodic_lengthscale, squared_sd,
+        for i in range(num_harmonics):
+            self.stats[idx + i] = np.array([freq * (i + 1), periodic_sd, periodic_lengthscale, squared_sd,
                                             squared_lengthscale])
 
-        self.num_harmonics_per_spectrum[power_spectrum_index] = curr_harmonic_index
+        self.num_harmonics_per_spectrum[power_spectrum_index] = num_harmonics
 
-    def delete_harmonics(self, power_spec_idx: int, first_harmonic: int, last_harmonic: int):
+    def delete_harmonics(self, power_spec_idx: int, first_harmonic: int, last_harmonic: int) -> None:
         idx = np.sum(self.num_harmonics_per_spectrum[:power_spec_idx])
         for i in reversed(range(first_harmonic, last_harmonic)):
             self.stats = np.delete(self.stats, idx + i, axis=0)
